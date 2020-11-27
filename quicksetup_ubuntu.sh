5414b216584894dab23c13ff794932ea6978339d
Fox Chen <foxhlchen@gmail.com>
2020-11-27 10:02:29 +0800

    add quick setup script for benchmark setting up on cloud machines

---
+++ quicksetup_ubuntu.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install -y build-essential vim git cscope libncurses-dev libssl-dev bison flex emacs git-mail
+sudo apt install -y  wget linux-tools-common linux-tools-generic linux-tools-`uname -r`
+
+cd ~
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.9.11.tar.xz
+tar xf linux-5.9.11.tar.xz
