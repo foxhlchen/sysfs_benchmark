68e32ab22d21eb9662dc818e985b5cef18bbe1f5
foxhlchen <foxhlchen@gmail.com>
2020-11-27 15:08:25 +0800

    Update quicksetup_ubuntu.sh

--- quicksetup_ubuntu.sh
+++ quicksetup_ubuntu.sh
@@ -3,7 +3,7 @@ sudo apt install -y build-essential vim git cscope libncurses-dev libssl-dev bis
 sudo apt install -y  wget linux-tools-common linux-tools-generic linux-tools-`uname -r`
 
 cd ~
-//wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.9.11.tar.xz
-//tar xf linux-5.9.11.tar.xz
+#wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.9.11.tar.xz
+#tar xf linux-5.9.11.tar.xz
 
 git clone https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
