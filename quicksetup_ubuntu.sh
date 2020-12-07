5576fd69cedc8f8696cdf1cc06e4b119d7d7258a
foxhlchen <foxhlchen@gmail.com>
2020-12-07 13:58:09 +0800

    Add libslang2-dev
    Add libslang2-dev in order to support perf interactive UI

--- quicksetup_ubuntu.sh
+++ quicksetup_ubuntu.sh
@@ -1,6 +1,8 @@
 sudo apt update
-sudo apt install -y build-essential vim git cscope libncurses-dev libssl-dev bison flex emacs
+sudo apt install -y build-essential vim git cscope libncurses-dev libssl-dev bison flex emacs libslang2-dev
 sudo apt install -y  wget linux-tools-common linux-tools-generic linux-tools-`uname -r`
+
+ln -s ../linux/tools/perf/perf .
 
 cd ~
 #wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.9.11.tar.xz
