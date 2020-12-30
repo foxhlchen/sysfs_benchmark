d436182199e7cb7eb87e21d321931fb59483e108
Fox Chen <foxhlchen@gmail.com>
2020-12-30 09:49:06 +0800

    Add update-grub operation

--- rm_kernel.sh
+++ rm_kernel.sh
@@ -9,13 +9,21 @@ read -p "which> " opt_idx
 opt=${modules[$opt_idx]}
 
 echo "To delete $opt?"
-
-read -p "y/N >" comfirm
+read -p "y/N > " comfirm
 
 if [[ $comfirm == "y" ]]; then
     echo "Deleting $opt"
-    sudo rm -rf /lib/modules/$opt && sudo rm -f /boot/*${opt}* && echo "Done" || echo "Failed!"
+    sudo rm -rf /lib/modules/$opt && sudo rm -f /boot/*${opt}* && echo "Done"
 fi
 
+if [[ $? != 0 ]]; then
+    echo "Failed!"
+    exit $?
+fi
 
-    
+echo "Run update-grub"
+read -p "y/N > " comfirm
+
+if [[ $comfirm == "y" ]]; then
+    sudo update-grub && echo "Done" || echo "Failed!"
+fi
